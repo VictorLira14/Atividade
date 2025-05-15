70b376c806c541c3bf5c05be6d976c13cb1809a9
VictorLira14 <159541935+VictorLira14@users.noreply.github.com>
2025-05-15 09:46:27 -0300

    Add files via upload

--- atvd2.js
+++ atvd2.js
@@ -1,24 +1,33 @@
 class Produto{
-    constructor(nome, preco, quantidade){
+    constructor(nome,preco,quantidade){
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
-    }
-
+}
 mostrarDados(){
-    console.log(`produtos ${this.nome}, preco${this.preco}, quantidade ${this.quantidade}`);
-    
-}
-
-vender(qnt){
-    if(qnt <= this.quantidade)
-        this.quantidade -= qnt;
-    console.log(`venda realizada. quantidade atual ${this.quantidade}`)
-
+    console.log(`O produto ${this.nome} custa ${this.preco} e tem ${this.quantidade} no estoque`);
 
 }
+vender(qtd){
+    if(qtd <= this.quantidade){
+        this.quantidade -= qtd;
+        console.log(`Venda realizada, restou ${this.quantidade} quantidades em estoque`);
+
+    }
+}
+repor(qtd){
+    this.quantidade += qtd;
+    console.log(`Quantidade em estoque ${this.quantidade} quantidades`);
+}
 
 }
+
+let produto = new Produto("lapis",2.6,100);
+produto.mostrarDados();
+//produto.vender(40);
+//produto.repor(20);
+
+
 
 
 
